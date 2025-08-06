0618e1e8c91d00ebb266e33ad73c66e2a2fa4eb7
YOUSUF_IBRAHIM <yousufibrahim8710@gmail.com>
2025-08-06 07:56:42 +0600

    solutions

--- C_Beautiful_Numbers.cpp
+++ C_Beautiful_Numbers.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-const int mod=1e9+7;
-const int MAX=1e6+5;
+const int mod = 1e9 + 7;
+const int MAX = 1e6 + 5;
 /*
         Yousuf Ibrahim
 
@@ -10,12 +10,72 @@ const int MAX=1e6+5;
 
         Stay Hungry;
 */
+int fact[MAX];
+int a, b, n;
+int power(int x, int n)
+{
+      if (n == 0)
+      {
+            return 1 % mod;
+      }
+      int cur = power(x, n / 2) % mod;
+      if (n % 2 == 0)
+      {
+            return 1ll * cur * cur % mod;
+      }
+      else
+      {
+            return 1ll * cur * cur % mod * x % mod;
+      }
+}
 
+int inverse(int b)
+{
+      return power(b, mod - 2);
+}
+
+int combi(int n, int k)
+{
+      if (k > n)
+            return 0;
+      int up = fact[n];
+      int down = 1ll * fact[k] * fact[n - k] % mod;
+      return 1ll * up * inverse(down) % mod;
+}
+
+bool is_good(int x)
+{
+      while (x)
+      {
+            int digit = x % 10;
+            if (digit != a and digit != b)
+            {
+                  return false;
+            }
+            x /= 10;
+      }
+      return true;
+}
 void solve()
 {
-      int a,b,n;
-      cin>>a>>b>>n;
-
+      fact[0] = 1;
+      for (int i = 1; i < MAX; i++)
+      {
+            fact[i] = 1ll * fact[i - 1] * i % mod;
+      }
+      cin >> a >> b >> n;
+      int ans = 0;
+      for (int k = 0; k <= n; k++)
+      {
+            int digit_sum = k * a + (n - k) * b;
+            if (is_good(digit_sum))
+            {
+                  int ways = combi(n, k);
+                  ans += ways;
+                  ans %= mod;
+            }
+      }
+      cout << ans << '\n';
 }
 
 signed main()
@@ -23,7 +83,7 @@ signed main()
       ios_base::sync_with_stdio(false);
       cin.tie(NULL);
       int t = 1;
-      //cin >> t;
+      // cin >> t;
       while (t--)
             solve();
       return 0;
